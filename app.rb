a017d6ddc8d341ecd966802d0fca112e91ad71dd
Bryce B <bryce.boddy@gmail.com>
2020-07-05 08:28:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
   end
   
   post '/checkout' do
-    session[:cart] = params[:cart]
+    session[:item] = params[:item]
     @session = session
     erb :checkout
   end
